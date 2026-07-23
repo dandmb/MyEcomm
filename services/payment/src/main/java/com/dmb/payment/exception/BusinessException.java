d965a9bca6dea5a86e0e0ad2f3703a7c93b2d1b9
dandmb <danbizwa@gmail.com>
2026-07-23 15:45:25 +0200

    payment service added

---
+++ services/payment/src/main/java/com/dmb/payment/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.dmb.payment.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BusinessException extends RuntimeException {
+
+  private final String msg;
+}
---
+++ services/payment/src/main/java/com/dmb/payment/notification/PaymentNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.dmb.payment.notification;
+
+
+import com.dmb.payment.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
