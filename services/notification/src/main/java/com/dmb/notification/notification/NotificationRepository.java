4a3a7b7ce1dac63b98c214f819a28ff983a95e08
dandmb <danbizwa@gmail.com>
2026-07-23 17:30:40 +0200

    notification service added

---
+++ services/notification/src/main/java/com/dmb/notification/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.dmb.notification.notification;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
